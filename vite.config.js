f851e9ba73b0e769118f6a2dbdfb0ce59b4f0c71
Ravikant Pal <iamravikantpal@gmail.com>
2026-05-03 10:28:35 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/rentizo/',
   plugins: [
     react(),
     VitePWA({
